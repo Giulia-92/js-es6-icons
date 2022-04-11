bad3263d1e55629af211eb773c8d5c12afb34b07
Giulia-92 <cionco.giulia@gmail.com>
2022-04-11 19:47:40 +0200

    js-es6-icons

--- data.js
+++ data.js
@@ -1,4 +1,4 @@
-[
+ const elementi = [
 	{
 		name: 'cat',
 		prefix: 'fa-',
@@ -114,9 +114,57 @@
 ];
 
 
-
+let coso = ''; //Variabile usata per salvare il valore del menu a tendina
 const selectElement = document.querySelector('.filterlist');
 selectElement.addEventListener('change', (event) => {
   const result = document.querySelector('.risultato');
-  result.textContent = `Hai scelto ${event.target.value}`;
+  coso = document.querySelector(".filterlist").value;
+  console.log(coso);
+  result.textContent = `Hai scelto : ${event.target.value}`;
+  stampaElementi();
+  
 });
+
+const teamContainer = document.querySelector('.container');
+
+function stampaElementi(){
+	const row = teamContainer.getElementsByClassName('.row mt-3');
+    let item = '';
+	
+    for(let i = 0; i < elementi.length; i++ ){
+		if (coso == elementi[i].type){
+        item +=  ` 
+        
+              <div class="col-1">
+                  <i class="fa-solid ${elementi[i].prefix}${elementi[i].name}" style="color:${elementi[i].color}"></i>
+                    <p>${elementi[i].name}</p>
+                </div>
+        
+			  `} 
+
+		if (coso == 'All'){
+        item +=  ` 
+        
+              <div class="col-1">
+			  <i class="fa-solid ${elementi[i].prefix}${elementi[i].name}" style="color:${elementi[i].color}"></i>
+                    <p>${elementi[i].name}</p>
+                </div>
+        
+			  `} 		
+
+			}
+	//teamContainer.append(row); 
+    teamContainer.innerHTML = item; 
+}
+
+
+
+
+/*
+
+document.getElementById("filterlist").innerHTML = elementi.filter(sceltaElementi);
+
+function sceltaElementi(elementi) {
+  return elementi.animal;
+}*/
+
